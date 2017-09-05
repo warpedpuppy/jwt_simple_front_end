08f317c72b81c4294f2d909b4eb052a90cd7b70a
System <none>
2017-09-05 16:23:26 +0000

    Import of Thinkful-Ed/node-jwt-auth

--- server.js
+++ server.js
@@ -5,6 +5,13 @@ const mongoose = require('mongoose');
 const morgan = require('morgan');
 const passport = require('passport');
 
+// Here we use destructuring assignment with renaming so the two variables
+// called router (from ./users and ./auth) have different names
+// For example:
+// const actorSurnames = { james: "Stewart", robert: "De Niro" };
+// const { james: jimmy, robert: bobby } = actorSurnames;
+// console.log(jimmy); // Stewart - the variable name is jimmy, not james
+// console.log(bobby); // De Niro - the variable name is bobby, not robert
 const {router: usersRouter} = require('./users');
 const {router: authRouter, basicStrategy, jwtStrategy} = require('./auth');
 
@@ -18,14 +25,14 @@ const app = express();
 app.use(morgan('common'));
 
 // CORS
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  if (req.method === 'OPTIONS') {
-    return res.send(204);
-  }
-  next();
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
+    if (req.method === 'OPTIONS') {
+        return res.send(204);
+    }
+    next();
 });
 
 app.use(passport.initialize());
@@ -36,7 +43,8 @@ app.use('/api/users/', usersRouter);
 app.use('/api/auth/', authRouter);
 
 // A protected endpoint which needs a valid JWT to access it
-app.get('/api/protected',
+app.get(
+    '/api/protected',
     passport.authenticate('jwt', {session: false}),
     (req, res) => {
         return res.json({
@@ -46,7 +54,7 @@ app.get('/api/protected',
 );
 
 app.use('*', (req, res) => {
-  return res.status(404).json({message: 'Not Found'});
+    return res.status(404).json({message: 'Not Found'});
 });
 
 // Referenced by both runServer and closeServer. closeServer
@@ -54,40 +62,40 @@ app.use('*', (req, res) => {
 let server;
 
 function runServer() {
-  return new Promise((resolve, reject) => {
-    mongoose.connect(DATABASE_URL, err => {
-      if (err) {
-        return reject(err);
-      }
-      server = app.listen(PORT, () => {
-        console.log(`Your app is listening on port ${PORT}`);
-        resolve();
-      })
-      .on('error', err => {
-        mongoose.disconnect();
-        reject(err);
-      });
+    return new Promise((resolve, reject) => {
+        mongoose.connect(DATABASE_URL, err => {
+            if (err) {
+                return reject(err);
+            }
+            server = app
+                .listen(PORT, () => {
+                    console.log(`Your app is listening on port ${PORT}`);
+                    resolve();
+                })
+                .on('error', err => {
+                    mongoose.disconnect();
+                    reject(err);
+                });
+        });
     });
-  });
 }
 
 function closeServer() {
-  return mongoose.disconnect().then(() => {
-     return new Promise((resolve, reject) => {
-       console.log('Closing server');
-       server.close(err => {
-           if (err) {
-               return reject(err);
-           }
-           resolve();
-       });
-     });
-  });
+    return mongoose.disconnect().then(() => {
+        return new Promise((resolve, reject) => {
+            console.log('Closing server');
+            server.close(err => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve();
+            });
+        });
+    });
 }
 
 if (require.main === module) {
-  runServer().catch(err => console.error(err));
-};
+    runServer().catch(err => console.error(err));
+}
 
 module.exports = {app, runServer, closeServer};
-
